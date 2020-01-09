8c4454855e982b453ffca194097e424907f7aea2
YraDokienko <uradokienko@gmail.com>
2020-01-09 02:53:13 +0200

    PjP-21 Added tests for ShippingForm

--- project/pizza/tests.py
+++ project/pizza/tests.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
 from django.urls import resolve, reverse
+from .forms import ShippingOrderForm
+from .models import ShippingOrder
 
 
 class PizzaTestCase(TestCase):
@@ -28,3 +30,36 @@ class PizzaTestCase(TestCase):
         self.assertEqual(url, '/pizza-update/75/edit/')
         resol = resolve(url)
         self.assertEqual(resol.view_name, 'pizza_update')
+
+
+class ShippingTestCase(TestCase):
+
+    def setUp(self) -> None:
+        pass
+
+    def test_page_shipping(self):
+        response = self.client.get('/cart/shipping/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'shipping.html')
+
+    # Valid Form
+    def test_shipping_form_valid(self):
+
+        form = ShippingOrderForm(data={
+            "first_name": 'Vova', "last_name": 'Petrenko', "email": 'r@gmail.com',
+            "phone": 380501234567, "city": 'Odessa', "street": 'Torgova', "house": '20',
+            "apartment": '13', "front_door": 1, "floor": 5, "number_persons": 5,
+            "comment": 'go to', "type_payment": 'card',
+        })
+        self.assertTrue(form.is_valid())
+
+    # Invalid Form
+    def test_shipping_form_invalid(self):
+
+        form = ShippingOrderForm(data={
+            "first_name": 'Vova', "last_name": 'Petrenko', "email": ' ',
+            "phone": 'ERROR', "city": 'Odessa', "street": 'Torgova', "house": '20',
+            "apartment": '13', "front_door": 1, "floor": 5, "number_persons": 5,
+            "comment": 'go to', "type_payment": 'card',
+        })
+        self.assertFalse(form.is_valid())
